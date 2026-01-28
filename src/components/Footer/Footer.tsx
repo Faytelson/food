af58211261d3ac2b882113ea6a6f7a99898115e6
Darya Faitelson <daryafaitelson@gmail.com>
2026-01-28 20:24:27 +0300

    fix(footer): removed unnecessary color prop

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = ({ className }: { className?: string }) => {
   return (
     <footer className={clsx(styles.footer, className)}>
       <div className={styles["footer__container"]}>
-        <Text className={styles["footer__info"]} color="white">Блог Натальи Русановой, 2018 - 2026.</Text>
+        <Text className={styles["footer__info"]} >Блог Натальи Русановой, 2018 - 2026.</Text>
         <Text className={styles["footer__info"]}>Макет: KTS, разработка: Darya Faytelson.</Text>
       </div>
     </footer>
