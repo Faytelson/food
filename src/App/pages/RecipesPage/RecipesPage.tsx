24f449a58baae1d49a0749db07d598c9ff22a95f
Darya Faitelson <daryafaitelson@gmail.com>
2026-01-30 17:14:30 +0300

    fix(img): changed recipe intro image dimensions

--- src/App/pages/RecipesPage/RecipesPage.tsx
+++ src/App/pages/RecipesPage/RecipesPage.tsx
@@ -3,7 +3,9 @@ import IntroSection from "@components/IntroSection";
 import RecipeSearchSection from "./components/RecipeSearchSection";
 import styles from "./RecipesPage.module.scss";
 import clsx from "clsx";
-import IntroImage from "@assets/images/food_page_intro_lg.png";
+import IntroImageLG from "@assets/images/food_page_intro_lg.png";
+import IntroImageSM from "@assets/images/food_page_intro_sm.png";
+import IntroImageXS from "@assets/images/food_page_intro_xs.png";
 
 export type RecipesPageProps = {
   className?: string;
@@ -13,7 +15,10 @@ const RecipesPage: React.FC<RecipesPageProps> = ({ className }) => {
   return (
     <main className={clsx(className, styles["recipes-page"])}>
       <IntroSection
-        image={{ title: "Recipes Intro", url: IntroImage }}
+        image={{
+          title: "Картинка с разными блюдами",
+          dimensions: { imgUrl_lg: IntroImageLG, imgUrl_sm: IntroImageSM, imgUrl_xs: IntroImageXS },
+        }}
         className={styles["recipes-page__intro-section"]}
       >
         Я подготовила для вас свои лучшие рецепты: здесь собраны блюда от будничных обедов до
